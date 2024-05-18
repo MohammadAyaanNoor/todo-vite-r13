dac5b888a8be9002aec41db6387b15685d31e285
Mohammad Ayaan Noor <rjayaan722@gmail.com>
2024-05-19 01:06:23 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,12 @@ import Header from "./components/Header";
 import Create from "./components/Create";
 import Show from "./components/Show";
 
-const App = () => {
-<<<<<<< HEAD
-    const [tasks, settasks] = useState(
-        JSON.parse(localStorage.getItem("tasks")) || []
-    );
-
-    return (
-        <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
-            {/*  */}
-            <Header tasks={tasks} />
-            {/*  */}
-            <Create tasks={tasks} settasks={settasks} />
-            {/*  */}
-            <Show tasks={tasks} settasks={settasks} />
-=======
-    
-    
+const App = () => {    
     return (
         <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
             <Header/>
             <Create/>
             <Show/>
->>>>>>> 397d0f5 (first commit)
         </div>
     );
 };
